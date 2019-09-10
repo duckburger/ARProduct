2d329d6af52947aeb43952991386c6430bbe5785
Kirill M <kmuravskiy@gmail.com>
2019-09-10 11:20:36 -0400

    Added a bunch of camera background materials.

---
+++ Assets/Scripts/AR/ARCamMaterialSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+ 
+[RequireComponent(typeof(ARCameraBackground))]
+public class ARCamMaterialSwitcher : MonoBehaviour
+{
+    public Material _ARCoreBackground;
+    public Material _ARKitBackground;
+ 
+    void Start ()
+    {
+        ARCameraBackground arcbg = GetComponent<ARCameraBackground> ();
+        arcbg.useCustomMaterial = true;
+#if UNITY_IOS
+        arcbg.customMaterial = _ARKitBackground;
+#else
+        arcbg.customMaterial = _ARCoreBackground;
+#endif
+    }
+}
+ 
