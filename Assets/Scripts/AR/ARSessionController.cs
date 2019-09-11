a00eb70fd001924d3afd2db13813272baf57f75d
KMurawkski <>
2019-09-11 13:08:28 -0400

    Update

---
+++ Assets/Scripts/AR/ARSessionController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARSessionController : MonoBehaviour
+{
+    //[SerializeField] ARSession arSession;
+    [SerializeField] GameObject arContentsPrefab;
+    GameObject currentARContents;
+
+    private void OnEnable()
+    {
+        currentARContents = Instantiate(arContentsPrefab, transform);
+        //arSession?.Reset();
+    }
+
+    private void OnDisable()
+    {
+        if (currentARContents)
+            Destroy(currentARContents);
+        currentARContents = null;
+    }
+}
---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    [SerializeField] GameObject mainObject;
+    [SerializeField] float rotationSpeed = 10f;
+    Vector3 rotSpeed;
+
+    private void Update()
+    {
+        if (mainObject)
+        {
+            RotateObject();
+            ApplyRotation();
+        }           
+    }
+
+
+    void RotateObject()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            rotSpeed = new Vector3(0, Input.GetAxis("Mouse X"), 0);
+        }
+        else if (Input.touchCount > 0)
+        {
+            rotSpeed = new Vector3(0, Input.GetAxis("Mouse X"), 0);
+        }
+    }
+
+    void ApplyRotation()
+    {
+        rotSpeed = Vector3.Lerp(rotSpeed, Vector3.zero, Time.deltaTime * 5f);
+        mainObject.transform.Rotate(rotSpeed * rotationSpeed, Space.World);
+    }
+}
